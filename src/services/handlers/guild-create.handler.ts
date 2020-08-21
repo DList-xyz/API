9784b2dcea91dde9f7259cd926cba87f5deba8a2
theADAMJR <adamjf123@gmail.com>
2020-08-21 11:52:10 +0100

    add welcome message

--- src/services/handlers/guild-create.handler.ts
+++ src/services/handlers/guild-create.handler.ts
@@ -1,14 +1,31 @@
 import EventHandler from './event-handler';
 import Deps from '../../utils/deps';
-import { Guild } from 'discord.js';
+import { Guild, MessageEmbed } from 'discord.js';
 import Guilds from '../../data/guilds';
+import config from '../../../config.json';
 
 export default class GuildCreateHandler implements EventHandler {
     on = 'guildCreate';
 
     constructor(private guilds = Deps.get<Guilds>(Guilds)) {}
 
-    invoke(guild: Guild) {
-        return this.guilds.get(guild);
+    async invoke(guild: Guild) {
+        await this.guilds.get(guild);
+
+        try {
+            await this.sendJoinMessage(guild);
+        } catch {}
+    }
+
+    sendJoinMessage(guild: Guild) {
+        return guild.systemChannel
+            ?.send(new MessageEmbed({
+                title: `Hey!`,
+                fields: [
+                    { name: ':rocket: Check out your server listing...', value: `${config.dashboardURL}/servers/${guild.id}` },
+                    { name: ':art: Customize it at...', value: `${config.dashboardURL}/dashboard/servers/${guild.id}/edit` }
+                ],
+                thumbnail: { url: guild.iconURL({ dynamic: true, size: 64 }) }
+            }));
     }
 }
